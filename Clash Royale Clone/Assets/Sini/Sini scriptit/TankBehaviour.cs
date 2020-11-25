c439f02dd8ee69562ef2ec43a5d5accdebc33bd0
Hiili88 <sini.karhunen@gmail.com>
2020-11-25 17:15:47 +0200

    .

---
+++ Clash Royale Clone/Assets/Sini/Sini scriptit/TankBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TankBehaviour : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public List<GameObject> waypointsGo;
+    public List<Transform> waypoints;
+    int nextPoint = 0;
+    public float patrolSpeed = 1f;
+    float waypointTolerance = 1f;
+
+    private Towers towerhp;
+
+    void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        nextPoint = ClosestPoint();
+        towerhp = waypointsGo[nextPoint].GetComponent<Towers>();
+
+    }
+
+    public int ClosestPoint() {
+        // this works, finds the closest point
+        int j = 0;
+        var smallest = Vector3.Distance(agent.transform.position, waypoints[0].transform.position);
+        for (var i = 1; i < waypoints.Count; i++) {
+            var dist = Vector3.Distance(agent.transform.position, waypoints[i].transform.position);
+            if (dist < smallest) {
+                smallest = dist;
+                j = i;
+            }
+        }
+        return j;
+    }
+
+}// class
--- Clash Royale Clone/Assets/Sini/Sini scriptit/UnitStats.cs
+++ Clash Royale Clone/Assets/Sini/Sini scriptit/UnitStats.cs
@@ -1,22 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-
-[CreateAssetMenu(fileName = "New UnitType", menuName = "Unit")]
-
-public class UnitType : ScriptableObject {
+public class UnitStats : MonoBehaviour
+{
 
     public string unitTypeName;
     public int health;
     public int attackPower;
     public string playerID;
 
-    private Transform transform;
 
-    private void Awake() {
-        
-    }
 
 }
