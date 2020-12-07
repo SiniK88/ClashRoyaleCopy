f881685f9b975be365e7627333594f63975475a1
DESKTOP-B67THC4\sinik <sini.karhunen@gmail.com>
2020-12-07 14:37:06 +0200

    Added finished gard art to Final project and messed out with graphics settings + started particles and material effects in my own scene.

---
+++ Clash Royale Clone/Assets/Sini/ArtTesting/ArtScenestats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtScenestats : MonoBehaviour
+{
+
+    public int towerMaxHP = 100;
+    public int towercurHP = 100;
+    bool isDestroyed = false;
+
+
+    // Update is called once per frame
+    void Update() {
+        if (towerMaxHP <= 0) {
+            GetComponent<MeshRenderer>().enabled = false;
+            //Destroy(gameObject);
+            isDestroyed = true;
+        }
+    }
+
+    public void HurtEnemy(int damageToGive) {
+        towerMaxHP -= damageToGive;
+    }
+
+
+    public void SetMaxHealth() {
+        towerMaxHP = towercurHP;
+    }
+
+
+}
---
+++ Clash Royale Clone/Assets/Sini/ArtTesting/DissolveOnStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveOnStart : MonoBehaviour
+{
+    Material material;
+    bool isDissolving = false;
+    float fade = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update() {
+        isDissolving = true;
+
+        if(isDissolving) {
+            fade -= Time.deltaTime;
+
+            if(fade <= 0f) {
+                fade = 0f;
+                isDissolving = false;
+            }
+
+            material.SetFloat("_Fade", fade);
+        }
+    }
+
+
+}
