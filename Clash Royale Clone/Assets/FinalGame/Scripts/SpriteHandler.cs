0e2dfe42d7c336010b6bdf0df5d729ba6bdd6c74
DESKTOP-B67THC4\sinik <sini.karhunen@gmail.com>
2020-12-14 13:08:28 +0200

    PlayerBoard visuals

--- Clash Royale Clone/Assets/FinalGame/Scripts/SpriteHandler.cs
+++ Clash Royale Clone/Assets/FinalGame/Scripts/SpriteHandler.cs
@@ -22,9 +22,6 @@ public class SpriteHandler : MonoBehaviour
 
 
         if (aiScript.GetState() == AIstate.Navigate || aiScript.GetState() == AIstate.Aggro) {
-            //print("moving");
-            // Blue players units behave correctly up on both cameras now, but not red players units. 
-
             if(Mathf.Abs(agent.velocity.y) >= Mathf.Abs(agent.velocity.x)) {
                 animator[0].SetFloat("Move Y", agent.velocity.y);
                 animator[0].SetFloat("Move X", 0);
@@ -37,30 +34,12 @@ public class SpriteHandler : MonoBehaviour
                 animator[1].SetFloat("Move X", -agent.velocity.x);
                 animator[1].SetFloat("Move Y", 0);
             }
-
-            /*
-            // this one works weirdly. It turns units to left or right at the middle of the arena. 
-            animator[0].SetFloat("Move X", movement.position.x);
-            animator[0].SetFloat("Move Y", movement.position.y);
-            animator[1].SetFloat("Move X", movement.position.x);
-            animator[1].SetFloat("Move Y", movement.position.y);
-            */
-
         }
-
-        //Check if unit is moving ...
-        //Liikkuu paitsi jos AI.State = Attack TAI Stun TAI NoState
-        //Check if unit is attacking ...
-        //Onko unit AI.State = Attack?
-
-        //Rotaatio, bool isAttacking, bool isWalking -> oikea animaatio
 
         if (aiScript.GetState() == AIstate.Attack) {
             print("attack state works");
-            //if (animator[0].gameObject.activeSelf) {
                 animator[0].SetTrigger("Hit");
                 animator[1].SetTrigger("Hit");
-            //}
         }
 
     }
